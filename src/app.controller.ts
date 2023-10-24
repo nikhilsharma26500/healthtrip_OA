fcdedfcd70f2a5fc8013d62d9dbc7ff102f690bf
Nikhil <nikhilsharma26500@outlook.com>
2023-10-24 23:43:06 +0530

    working basic model of telegram bot

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
+import { BotService } from './bot/bot.service';
+// import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  // constructor(private readonly appService: AppService) {}
+
+  constructor(private readonly botService: BotService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getBotDialog(@Res() res: any) {
+    this.botService.botMessage();
+    res.status(HttpStatus.OK).send('Bot service has started!');
   }
 }
