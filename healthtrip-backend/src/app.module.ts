30d331209741a44e6381df29e416eb163991a0d3
Nikhil <nikhilsharma26500@outlook.com>
2023-10-27 21:07:26 +0530

    reconfigured the stack with next and nest

--- healthtrip-backend/src/app.module.ts
+++ healthtrip-backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BotService } from './bot/bot.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, BotService],
+  providers: [AppService],
 })
 export class AppModule {}
